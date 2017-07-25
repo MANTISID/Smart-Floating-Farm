9c49fc1c74aadf00b9c4b79c7b041f4df8959146
MANTIS ID <mantisid.ho@gmail.com>
2017-07-25 20:23:46 +0700

    Add files via upload

---
+++ SENSOR_CONFIGURATION.c
@@ -0,0 +1,28 @@
+
+
+/*
+ * UART          - 0 1
+ * LED HIJAU     - 2
+ * LED MERAH     - 3
+ * BUZZER        - 4
+ * DHT11         - 5
+ * USIRR         - 6
+ * ESP8266       - 7 8
+ * DS18B20       - 9
+ * RELAY MOTOR   - 10
+ * RELAY MOTOR   - 11
+ * 
+ * 
+ * VIBRATION     - A0
+ * LIGHT         - A1
+ * LED GREEN     - A2
+ * MQ02          - A3
+ * RTC           - A4 A5
+ * ADS1115       - A4 A5
+ * LCD I2C       - A4 A5
+ * WATER HEIGHT  - ADS0
+ * SOIL MOISTURE - ADS1
+ * 
+ * GYRO          - INTERNAL 101
+
+*/
